8e28362de8fdbe503bdfaf33d99e3068a7fda8dd
BronwenM <bronwen.marchant@gmail.com>
2024-11-24 18:20:21 -0800

    Setup Dashboard filestructure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,9 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import NavBar from './components/NavBar';
-import './App.css'
+import NavBar from './components/Navbar/Navbar';
+import './App.scss'
+import Dashboard from './components/Dashboard/Dashboard';
 
 function App() {
   const [message, setMessage] = useState('')
@@ -21,6 +22,7 @@ function App() {
       <Router>
       <NavBar />
       <h1>{message}</h1>
+        <Dashboard/>
       </Router>
     </>
   )
---
+++ frontend/src/components/Dashboard/Dashboard.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './dashboard.scss'
+
+const Dashboard = (props) => {
+  const {userRole} = props;
+  
+  return (
+    <section className='dashboard'>
+      <div>
+        Section
+      </div>
+    </section>
+  )
+}
+
+export default Dashboard
