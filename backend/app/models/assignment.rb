92acb138ced36b30d7e430803a2dbd5c0dadaeae
amusictheorist <a_millaresthomson@outlook.com>
2024-11-25 10:53:14 -0500

    Finish writing preliminary development seeds

--- backend/app/models/assignment.rb
+++ backend/app/models/assignment.rb
@@ -2,5 +2,5 @@ class Assignment < ApplicationRecord
   belongs_to :game
   belongs_to :official, class_name: 'User'
   belongs_to :assigner, class_name: 'User'
-  belongs_to :game_pamenty
+  belongs_to :game_payment
 end
