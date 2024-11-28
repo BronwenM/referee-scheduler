3159f9e510eb53e09f087d64d05195d8c2bd922b
amusictheorist <a_millaresthomson@outlook.com>
2024-11-28 11:55:31 -0500

    Add explanatory comments

--- backend/app/controllers/api/permissions_controller.rb
+++ backend/app/controllers/api/permissions_controller.rb
@@ -1,9 +1,12 @@
 class Api::PermissionsController < ApplicationController
+
+  #GET /permissions
   def index
     permissions = Permission.all
     render json: permissions
   end
 
+  #GET /permissions/:id
   def show
     permission = Permission.find(params[:id])
     render json: permission
--- backend/app/controllers/api/roles_controller.rb
+++ backend/app/controllers/api/roles_controller.rb
@@ -1,9 +1,12 @@
 class Api::RolesController < ApplicationController
+
+  #GET /roles
   def index
     roles = Role.all
     render json: roles
   end
 
+  #GET /roles/:id
   def show
     role = Role.find(params[:id])
     render json: role
