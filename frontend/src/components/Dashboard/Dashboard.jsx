8b6bf99a7292c24af88b9851842ef0c5ac20be27
BronwenM <bronwen.marchant@gmail.com>
2024-11-28 01:35:42 -0800

    Moved modal to context to persist data across components

--- frontend/src/components/Dashboard/Dashboard.jsx
+++ frontend/src/components/Dashboard/Dashboard.jsx
@@ -6,13 +6,13 @@ import { useModal } from '../../hooks/useModal';
 
 const Dashboard = (props) => {
   const {userRole} = props;
-  const {showModal, toggleModal} = useModal();
+  const {showModal} = useModal();
 
   return (
     <section className='dashboard'>
       <div className='dashboard__content'>
-        {showModal && <Modal toggleModal={toggleModal} content={ <AssignmentListItem gameDate={{day: 26, month: "June", year: 2024}} gameTime="6:30 PM" level="U17A" gameName="Islanders 09 vs Fury 08" field="Diamond 2" position="Plate"/>} />}
-        <AssignmentListItem gameDate={{day: 26, month: "June", year: 2024}} gameTime="6:30 PM" level="U17A" gameName="Islanders 09 vs Fury 08" field="Diamond 2" position="Plate" toggleModal={toggleModal}/>
+        {showModal && <Modal content={ <AssignmentListItem gameDate={{day: 26, month: "June", year: 2024}} gameTime="6:30 PM" level="U17A" gameName="Islanders 09 vs Fury 08" field="Diamond 2" position="Plate"/>} />}
+        <AssignmentListItem gameDate={{day: 26, month: "June", year: 2024}} gameTime="6:30 PM" level="U17A" gameName="Islanders 09 vs Fury 08" field="Diamond 2" position="Plate"/>
       </div>
     </section>
   )
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,12 +4,15 @@ import "./index.scss";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./hooks/useAuth.jsx";
+import { ModalProvider } from "./hooks/useModal.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <App />
+        <ModalProvider>
+          <App />
+        </ModalProvider>
       </AuthProvider>
     </BrowserRouter>
   </StrictMode>
