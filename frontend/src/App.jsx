667710983ae8e1525685a9c9e017d13e9e35cfd9
Saif Al-saedi <saif4004@gmail.com>
2024-11-28 17:59:22 -0800

    Add ActionController::Cookies to handle cookies../backend/app/controllers/api/sessions_controller.rb

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -54,7 +54,6 @@ function App() {
             <Route path="/profile" element={<h1>Profile</h1>} />
           </Route>
         </Routes>
-        <GameForm />
       </main>
       <Footer />
     </>
