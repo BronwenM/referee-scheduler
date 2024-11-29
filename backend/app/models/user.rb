ca7e6c716a119b84e3a96f1c10865e12773ac99a
BronwenM <bronwen.marchant@gmail.com>
2024-11-29 11:53:30 -0800

    Fixed user role getting from backend

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   has_secure_password
   belongs_to :user_association, class_name: 'Association'
   belongs_to :role
-  has_many :permissions, through: :role
+  has_many :role, through: :role
 end
